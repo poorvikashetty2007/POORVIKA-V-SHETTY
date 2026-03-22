6e5b12bddc7ff4c5571d15cc602b33f593f73208
POORVIKA V SHETTY <poorvikashetty2007@gmail.com>
2026-03-22 11:41:27 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,15 @@
+pi = 22/7
+print("Value of pi:", pi)
+print("Data type of pi:", type(pi))
+
+print("Cannot use 'for' as a variable name because it is a Python keyword.")
+principal = 10000   
+rate = 5            
+time = 3           
+
+simple_interest = (principal * rate * time) / 100
+
+print("Principal:", principal)
+print("Rate:", rate)
+print("Time:", time)
+print("Simple Interest:", simple_interest)
