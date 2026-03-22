93462ec3e63925cd6a84a813216f2be66e245a13
POORVIKA V SHETTY <poorvikashetty2007@gmail.com>
2026-03-22 11:45:46 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,18 @@
+justice_league = ["Superman", "Batman", "Wonder Woman", "Flash", "Aquaman", "Green Lantern"]
+print("Initial Justice League:", justice_league)
+print("Number of members:", len(justice_league))
+justice_league.append("Batgirl")
+justice_league.append("Nightwing")
+print("\nAfter adding Batgirl and Nightwing:", justice_league)
+justice_league.remove("Wonder Woman")
+justice_league.insert(0, "Wonder Woman")
+print("\nAfter making Wonder Woman the leader:", justice_league)
+justice_league.remove("Green Lantern")
+flash_index = justice_league.index("Flash")
+justice_league.insert(flash_index, "Green Lantern")
+print("\nAfter separating Aquaman and Flash:", justice_league)
+justice_league = ["Cyborg", "Shazam", "Hawkgirl", "Martian Manhunter", "Green Arrow"]
+print("\nNew Justice League team:", justice_league)
+justice_league.sort()
+print("\nAlphabetically sorted Justice League:", justice_league)
+print("\nNew leader (0th index):", justice_league[0])
