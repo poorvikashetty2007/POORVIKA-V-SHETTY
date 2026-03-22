1f474540f60dfe3783f03b2b308ab524e2253e89
POORVIKA V SHETTY <poorvikashetty2007@gmail.com>
2026-03-22 11:46:13 +0530

    Add files via upload

---
+++ if condition1.py
@@ -0,0 +1,11 @@
+height = float(input("Enter height in meters: "))
+weight = float(input("Enter weight in kilograms: "))
+bmi = weight / (height ** 2)
+if bmi >= 30:
+    print("Obesity")
+elif bmi >= 25 and bmi < 30:
+    print("Overweight")
+elif bmi >= 18.5 and bmi < 25:
+    print("Normal")
+else:
+    print("Underweight")
